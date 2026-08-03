c7c3c6216d6ebb7dd8545b6acba5e1ac823ebe03
Bingo666 <ou5156@163.com>
2026-08-03 14:43:27 +0800

    update site

--- src/sites.ts
+++ src/sites.ts
@@ -60,6 +60,7 @@ export const pinnedSites: Site[] = [
   { name: 'liwenzhou', url: 'https://www.liwenzhou.com/' },
   { name: 'Youlai', url: 'https://www.youlai.tech/vue-docs/' },
   { name: 'deepseek', url: 'https://chat.deepseek.com/' },
+  { name: '硅基流动', url: 'https://www.siliconflow.cn/' },
   { name: '豆包', url: 'https://www.doubao.com/' },
   { name: 'coze', url: 'https://www.coze.cn/' },
   { name: 'dify', url: 'https://dify.ai/zh' },
